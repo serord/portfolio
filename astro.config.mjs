e0dcc77daf4d2c9c89b7ca57a9813f13d2959132
sergey <33asasergey33@gmail.com>
2025-05-26 22:22:14 +0300

    fix: dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
-import sitemap from '@astrojs/sitemap'
-import { remarkReadingTime } from './src/utils/remarkReadingTime.ts'
-import remarkUnwrapImages from 'remark-unwrap-images'
-import rehypeExternalLinks from 'rehype-external-links'
 import expressiveCode from 'astro-expressive-code'
 import { expressiveCodeOptions } from './src/site.config'
+import node from '@astrojs/node'
 import icon from 'astro-icon'
 
 // https://astro.build/config
@@ -17,27 +13,11 @@ export default defineConfig({
 		tailwind({
 			applyBaseStyles: false
 		}),
-		sitemap(),
-		mdx(),
 		icon()
 	],
-	markdown: {
-		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
-		rehypePlugins: [
-			[
-				rehypeExternalLinks,
-				{
-					target: '_blank',
-					rel: ['nofollow, noopener, noreferrer']
-				}
-			]
-		],
-		remarkRehype: {
-			footnoteLabelProperties: {
-				className: ['']
-			}
-		}
-	},
 	prefetch: true,
 	output: 'server',
+	adapter: node({
+		mode: 'standalone'
+	})
 })
